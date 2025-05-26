ba839d920656f3082b2bb8145c55365d73a52b85
chuyjr10 <chuy8925@gmail.com>
2025-05-25 20:08:25 -0700

    Merge remote-tracking branch 'origin/main'

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
-# File with constants
+# File with constants URBAN_ROUTES_URL
+# URL for the Urban Routes app
+URBAN_ROUTES_URL = ''
+
+# Test data
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+
